7407b14c79ac8d046e15a9dd535279f9e800cbe5
1659009 <1659009@itec.hcmus.edu.vn>
2018-02-01 16:11:52 +0700

    phan so

--- js/phanso_script.js
+++ js/phanso_script.js
@@ -1,23 +1,36 @@
 function calculate() {
 	var result;
-	var ps1 = Number(document.getElementById("i1"));
-	var ps2 = Number(document.getElementById("i2"));
-	var operator = document.getElementById("ope");
+	var ps1 = document.getElementById("i1").value;
+	var ps2 = document.getElementById("i2").value;
+	var operator = document.getElementById("ope").value;
+	var i;
+	var ps1_ts = parseInt(ps1.split("/")[0]);
+	var ps1_ms = parseInt(ps1.split("/")[1]);
+	var ps2_ts = parseInt(ps2.split("/")[0]);
+	var ps2_ms = parseInt(ps2.split("/")[1]);
+	var result_ts = 0;
+	var result_ms = 0;
 	if (operator === "plus") 
 	{
-		result = ps1 + ps2;
+		result_ts = (ps1_ts * ps2_ms) + (ps2_ts * ps1_ms);
+		result_ms = ps1_ms * ps2_ms;
 	}
 	else if (operator === "minus") 
 	{
-		result = ps1 - ps2;
+		result_ts = (ps1_ts * ps2_ms) - (ps2_ts * ps1_ms);
+		result_ms = ps1_ms * ps2_ms;
 	}
 	else if (operator === "multiply") 
 	{
-		result = ps1 * ps2;
+		result_ts = ps1_ts * ps2_ts;
+		result_ms = ps1_ms * ps2_ms;
 	}
 	else if (operator === "divide") 
 	{
-		result = ps1 / ps2;
+		result_ts = ps1_ts * ps2_ms;
+		result_ms = ps1_ms * ps2_ts;
 	}
-	document.getElementById("rs").innerHTML = result;
+
+	document.getElementById("rs").innerHTML = result_ts + "/" + result_ms;
+	console.log(ps1_ts + " -- " + ps1_ms + " -- " + operator + " -- " + ps2_ts + " -- " + ps2_ms );
 }
